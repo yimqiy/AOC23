dfc2709e9e5535d14f77d02056afe1e8c8cb731f
yimqiy <40708187+yimqiy@users.noreply.github.com>
2023-12-12 17:48:27 +0800

    :|

--- aoc12.py
+++ aoc12.py
@@ -4,35 +4,30 @@ def solve(pos,sets,length):
     if a[pos][sets][length]>=0:
         return a[pos][sets][length]
     if pos<0:
-        if not sets and  length<=pos+1:
-            return 1
-        else:
-            return 0
+        return (not sets and length<=pos+1)
     n = 0
     #no fill: copy last or complete a new set
-    if st[pos] == '.' or st[pos] == '?': 
-        if not length: #. after this
+    if st[pos] in '?.' and not length: 
             if sets:
                 n += solve(pos-1,sets-1,l[sets-1])
             n += solve(pos-1,sets,0)
-    if st[pos] == '#' or st[pos] == '?':
-        if length:
+    if st[pos] in '?#' and length:
             n += solve(pos-1,sets,length-1)
     a[pos][sets][length] = n
     return n
-N=0
-folder_path = 'C:\\Users\\yimqi\\Desktop\\Input.txt'
-p2 = True
 
+folder_path = 'C:\\Users\\yimqi\\Desktop\\Input.txt'
+P1=P2=0
 with open(folder_path, 'r') as file:
     for ln, line in enumerate(file):        
         l=(list(map(int,re.findall(r'-?\d+', line))))
-        if p2: l = l*5
         st = line.split()[0]
-        if p2:
-            st = (st+"?")*4 + st + '.'
-        m = max(l)
-        a = [[[-1]*(m+1) for j in range(len(l)+1)] for i in range(len(st))]
+        a = [[[-1]*(max(l)+1) for j in range(len(l)+1)] for i in range(len(st))]
         w = solve(len(st)-1,len(l),0)
-        N+=w
-print(N)
+        P1+=w
+        l = l*5
+        st = (st+"?")*4 + st + '.'
+        a = [[[-1]*(max(l)+1) for j in range(len(l)+1)] for i in range(len(st))]
+        w = solve(len(st)-1,len(l),0)
+        P2+=w
+print(P1,P2)
