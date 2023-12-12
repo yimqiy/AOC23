2a5cdf473a03fe74d88579a5b904145019b3ac15
yimqiy <40708187+yimqiy@users.noreply.github.com>
2023-12-12 17:52:33 +0800

    ;

--- aoc12.py
+++ aoc12.py
@@ -1,19 +1,16 @@
 import re
 #ways to have sets (one indexed) at pos (zero indexed) with length remaining
 def solve(pos,sets,length):
-    if a[pos][sets][length]>=0:
-        return a[pos][sets][length]
-    if pos<0:
-        return (not sets and length<=pos+1)
+    if (pos,sets,length) in a: return a[(pos,sets,length)]
+    if pos<0: return (not sets and length<=pos+1)
     n = 0
     #no fill: copy last or complete a new set
     if st[pos] in '?.' and not length: 
-            if sets:
-                n += solve(pos-1,sets-1,l[sets-1])
+            if sets:  n += solve(pos-1,sets-1,l[sets-1])
             n += solve(pos-1,sets,0)
     if st[pos] in '?#' and length:
             n += solve(pos-1,sets,length-1)
-    a[pos][sets][length] = n
+    a[(pos,sets,length)] = n
     return n
 
 folder_path = 'C:\\Users\\yimqi\\Desktop\\Input.txt'
@@ -22,12 +19,12 @@ with open(folder_path, 'r') as file:
     for ln, line in enumerate(file):        
         l=(list(map(int,re.findall(r'-?\d+', line))))
         st = line.split()[0]
-        a = [[[-1]*(max(l)+1) for j in range(len(l)+1)] for i in range(len(st))]
+        a = {}
         w = solve(len(st)-1,len(l),0)
         P1+=w
         l = l*5
         st = (st+"?")*4 + st + '.'
-        a = [[[-1]*(max(l)+1) for j in range(len(l)+1)] for i in range(len(st))]
+        a = {}
         w = solve(len(st)-1,len(l),0)
         P2+=w
 print(P1,P2)
